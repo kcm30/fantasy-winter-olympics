4f7fa8d9b50bfc1c6697f2a186af10b83e0987e1
Kevin Minkus <kcm30@pitt.edu>
2018-02-10 16:39:40 -0500

    Updated requirements

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import dash
 from dash.dependencies import Input, Output
 import dash_core_components as dcc
 import dash_html_components as html
-import plotly.graph_objs as goimport pandas as pd
+import pandas as pd
 
 app = dash.Dash()
 app.config['suppress_callback_exceptions']=True

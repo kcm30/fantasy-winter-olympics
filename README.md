cb0ce403987449c58c950154256352e03129f81a
Kevin Minkus <kcm30@pitt.edu>
2018-02-10 15:26:41 -0500

    Push app, data, and read me

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # fantasy-winter-olympics
-Web App for Family Winter Olympics Competition
+Web App to Track Family Winter Olympics Competition
+
+The rules for the competition are as follows:
+* Competitors must select 7 countries competing in the 2018 Winter Olympics
+* The seven countries, combined, must not have won more than 25 total medals in 2014.
+* Excluded from competition are the United States, Norway, Canada, the Netherlands, Olympic Athletes from Russia, and South Korea (as host).
+* At least three countries must not have won any medals in 2014.
+* The competitor whose countries win the most total medals is the champion.
+* In case of a tie, the following criteria will be used to determine the winner: 
+  1) Gold Medals; 
+  2) Silver Medals; 
+  3) Bronze Medals; 
+  4) Declare a tie.
+
---
+++ app.py
@@ -0,0 +1,98 @@
+import dash
+from dash.dependencies import Input, Output
+import dash_core_components as dcc
+import dash_html_components as html
+import plotly.graph_objs as go
+import pandas as pd
+
+app = dash.Dash()
+app.config['suppress_callback_exceptions']=True
+
+#########################
+### Data Manipulation ###
+#########################
+medal_count = pd.read_html('http://www.nbcolympics.com/medals')[0]
+teams = pd.melt(pd.read_json("country_selections.json"))
+teams.columns = ['Team', 'Country']
+data = pd.merge(teams, medal_count, how='left').fillna(0)
+data['Place'] = 0
+
+player_data = (data.groupby("Team")
+                   .sum()
+                   .reset_index()[['Place', 'Team', 'Total', 'Gold', 'Silver', 'Bronze']]
+                   .sort_values(['Total', 'Gold', 'Silver', 'Bronze'], ascending=False))
+player_data['Place'] = range(1, 8)
+
+country_data = (pd.merge(teams, medal_count, on='Country', how='outer')
+				  .fillna(0))
+country_data.loc[country_data.Place == 0, 'Place'] = country_data.Place.max()+1 
+
+
+# TODO: Maybe make the big table dependent on a dropdown where you select the person???
+
+#########################
+# Dashboard/Layout view #
+#########################
+
+
+def generate_table(dataframe):
+    return html.Table(
+        # Header
+        [html.Tr([html.Th(col) for col in dataframe.columns])] +
+
+        # Body
+        [html.Tr([
+            html.Td(dataframe.iloc[i][col]) for col in dataframe.columns
+        ]) for i in range(len(dataframe))],
+    )
+
+
+
+app = dash.Dash()
+
+app.layout = html.Div([
+	html.Div([
+    	html.H4(children='Minkus Family Standings'),
+    	generate_table(player_data)], className='six columns'),
+	html.Div([
+		html.H4(children="Country Standings"),
+		html.Label('View Team'),
+    	dcc.Dropdown(id='dropdown',
+        	options=[
+				{'label': 'All Countries', 'value': 'All Countries'},
+            	{'label': 'Dan', 'value': 'Dan'},
+            	{'label': 'Derek', 'value': 'Derek'},
+            	{'label': 'Gail', 'value': 'Gail'},
+				{'label': 'Jeff', 'value': 'Jeff'},
+				{'label': 'Kevin', 'value': 'Kevin'},
+				{'label': 'Laura', 'value': 'Laura'},
+				{'label': 'Sarah', 'value': 'Sarah'}
+        	],
+        	value='All Countries'
+    	), 
+		html.Div(id='output-table')
+		], className='six columns')
+])
+
+app.css.append_css({
+    'external_url': 'https://codepen.io/chriddyp/pen/bWLwgP.css'
+})
+
+@app.callback(
+    Output(component_id='output-table', component_property='children'),
+    [Input(component_id='dropdown', component_property='value')]
+)
+def generate_responsive_table(value):
+	dataframe = country_data
+	if value != 'All Countries':
+		dataframe = (dataframe[dataframe.Team == value].drop('Team', axis=1)
+                                                       .sort_values(['Total', 'Gold', 'Silver', 'Bronze'], ascending=False))
+	else:
+		dataframe = (dataframe.drop('Team', axis=1)
+                              .drop_duplicates()
+                              .sort_values(['Total', 'Gold', 'Silver', 'Bronze'], ascending=False))
+	return generate_table(dataframe)
+
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
